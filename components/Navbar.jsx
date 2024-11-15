11de4d19d8debc6b60ddd2bc281395910cb91436
jf <linzhoufang@gmail.com>
2024-11-15 00:36:16 -0800

    attempting to github page fix png issue

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     return (
         <>
             <nav>
-                <img src="../images/react-icon-small.png" />
+                <img src="./images/react-icon-small.png" />
                 <div className="nav--logo_text">ReactFacts</div>
                 <h4 className="nav--title">React Course - Project 1</h4>
             </nav>
